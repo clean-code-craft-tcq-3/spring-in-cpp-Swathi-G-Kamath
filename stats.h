31bc69344baec6eceec1e23b0672b75657c4cf3d
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-03-01 12:50:31 +0530

    Update stats.h

--- stats.h
+++ stats.h
@@ -37,6 +37,6 @@ class StatsAlerter
     std::vector<IAlerter*> al;
     
   public:
-     StatsAlerter(float , std::vector<__>& );
+     StatsAlerter(float , std::vector<IAlerter *>& );
      void checkandAlert(std::vector<float>& );
 };
