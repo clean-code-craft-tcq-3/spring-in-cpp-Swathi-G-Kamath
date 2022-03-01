03387bdb780a04c2ff98a1c72803c95e6b837361
Swathi-G-Kamath <swathikamath69@gmail.com>
2022-03-01 12:08:21 +0530

    Update stats.cpp

--- stats.cpp
+++ stats.cpp
@@ -1,5 +1,62 @@
 #include "stats.h"
+#include <algorithm>
+#include <cmath>
 
-Stats Statistics::ComputeStatistics(const std::vector<___>& ) {
-    //Implement statistics here
+struct Stats Statistics::ComputeStatistics(const std::vector<float>& num) {
+    
+    /*Creation of Stats object*/
+    Stats stat1;
+    
+    if(!num.empty())
+    {
+        for(auto i:num)
+        {
+            stat1.average+-i;
+        }
+    
+        stat1.average=stat1.average/num.size();
+        stat1.min=*min_element(num.begin(),num.end());
+        stat1.max=*max_element(num.begin(),num.end());
+    }
+        
+    return stat1;
+}
+
+EmailAlert::EmailAlert() 
+{ 
+    emailSent = false; 
+}
+
+void EmailAlert::SetAlert()
+{
+    this->emailSent = true;
+}
+
+LEDAlert::LEDAlert() 
+{ 
+    ledGlows = false; 
+}
+
+void LEDAlert::SetAlert()
+{
+    this->ledGlows = true; 
+}
+
+StatsAlerter::StatsAlerter(float f , std::vector<IAlerter*>& i)
+    :max(m) , alerter(al)
+{}
+
+void StatsAlerter::checkAndAlert(const std::vector<float>& n)
+{
+    for (auto i : n)
+    {
+        if (i > max)
+        { 
+            for (int j = 0; j < alerter.size(); ++j)
+            {
+                this->alerter.at(j)->SetAlert();
+            }
+            break;
+        }
+    }
 }
